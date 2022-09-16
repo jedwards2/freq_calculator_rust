3ca1b430d39dcf1cbcef3feaeb442dd57a15d749
Jack <jack.morgan.edwards@gmail.com>
2022-09-16 12:11:04 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let num = 55.0;
+    let mut list = Vec::new();
+    let mut uppers = upper_freqs(num);
+    let mut lowers = lower_freqs(num);
+
+    list.append(&mut lowers);
+    list.push(num);
+    list.append(&mut uppers);
+
+    println!("{:?}", list);
+}
+
+fn upper_freqs(mut num: f64) -> Vec<f64>{
+    let mut all_freqs = Vec::new();
+
+    while num * 2.0 < 20000.0 {
+        num *= 2.0;
+        all_freqs.push(num);
+    }
+
+    all_freqs
+}
+
+fn lower_freqs(mut num: f64) -> Vec<f64>{
+    let mut all_freqs = Vec::new();
+
+    while num / 2.0 > 20.0 {
+        num /= 2.0;
+        all_freqs.push(num);
+    }
+
+    all_freqs
+}
